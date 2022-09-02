2b89c6fd1b7f563ced40bfe7264f7eea3291be9a
ban898 <davidface2@gmail.com>
2022-09-02 11:19:13 +0300

    init commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = 'c78b1afa-e611-42e7-9d08-95a7ac62756b';
+export const MODAL_CLOSE_SEC = 2.5;
